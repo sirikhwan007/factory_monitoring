24753f15fcc83689ada2c9a02a056b0030687143
sirikhwan007 <sirikhwan.kh@rmuti.ac.th>
2026-01-29 20:25:16 +0700

    fix this

--- admin/edit_repair.php
+++ admin/edit_repair.php
@@ -87,7 +87,7 @@ $username = $_SESSION['username'] ?? 'ผู้ใช้งาน';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>จัดการงานซ่อม #<?= $repair_id ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="/factory_monitoring/assets/css/index.css">
+    <link rel="stylesheet" href="/factory_monitoring/admin/assets/css/index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
