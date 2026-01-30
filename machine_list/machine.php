7897eac6b6aa22b68ce6d545a439f3ddd26c8577
sirikhwan007 <sirikhwan.kh@rmuti.ac.th>
2026-01-31 01:10:46 +0700

    Fix script tag placement for SweetAlert2 in machine.php; improve HTML structure

--- machine_list/machine.php
+++ machine_list/machine.php
@@ -47,13 +47,12 @@ $conn->close();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>รายการเครื่องจักร</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link rel="stylesheet" href="/factory_monitoring/machine_list/css/machine_list.css">
   <link rel="stylesheet" href="/factory_monitoring/admin/assets/css/index.css">
   <link rel="stylesheet" href="/factory_monitoring/Manager/assets/css/Sidebar.css">
   <link rel="stylesheet" href="/factory_monitoring/Operator/assets/css/SidebarOperator.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <style>
     .dashboard {
       flex: 1;
