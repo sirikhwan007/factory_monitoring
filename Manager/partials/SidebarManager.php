b0a93460f03f5fc5b56ba5c2651afe2d96f685dc
sirikhwan007 <sirikhwan.kh@rmuti.ac.th>
2026-01-30 12:16:11 +0700

    feat: add machine list page with responsive layout, search functionality, and machine status updates

--- Manager/partials/SidebarManager.php
+++ Manager/partials/SidebarManager.php
@@ -47,7 +47,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
             </li>
 
             <li>
-                <a href="/factory_monitoring/admin/machines.php"
+                <a href="/factory_monitoring/manager/machines.php"
                     class="<?= $currentPage === 'machines.php' ? 'active' : '' ?>">
                     <i class="fas fa-industry"></i>
                     <span>สถานะเครื่องจักร</span>
