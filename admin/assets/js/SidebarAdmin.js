539f5166c908d284a2897d20a9893671415abd14
sirikhwan007 <sirikhwan.kh@rmuti.ac.th>
2026-03-25 17:40:08 +0700

    updateadmin

--- admin/assets/js/SidebarAdmin.js
+++ admin/assets/js/SidebarAdmin.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
-  // เมื่อคลิกปุ่ม Hamburger
+
   $(".btn-hamburger").on("click", function () {
     $(".sidebar-wrapper").toggleClass("active");
     $(".sidebar-overlay").toggleClass("active");
   });
 
-  // เมื่อคลิกที่ Overlay ให้ปิด Sidebar
   $(".sidebar-overlay").on("click", function () {
     $(".sidebar-wrapper").removeClass("active");
     $(this).removeClass("active");
